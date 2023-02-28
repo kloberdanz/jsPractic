049e6c3c9002a5c330e7a54d702add22cc3ac8a7
Kloberdanz Lucas <107454132+kloberdanz@users.noreply.github.com>
2023-02-28 03:18:42 -0300

    fix in typing error
    unsuitable language

--- asset/main.js
+++ asset/main.js
@@ -1,16 +1,16 @@
 // const pregunta = prompt('¿Cuánto es 2 + 2?');
 // const respuesta = pregunta;
 
-// const como = ()=> pregunta == 4 ? document.getElementById('r').innerHTML= "chota" : console.warn("confuso");
+// const como = ()=> pregunta == 4 ? document.getElementById('r').innerHTML= "con todo" : console.warn("confuso");
 
-// function Orto(respuesta){
-//     if(pregunta == 4) console.log("felicidades, sumas al nivel de un perro")
+// function Otra(respuesta){
+//     if(pregunta == 4) console.log("felicidades, sumas al nivel de un perro, los perros no suman.")
 //     else console.warn("apestas, tu intelecto es aun peor que el de un insecto," + respuesta + " , no es 4");
 // }
 // como();
-// Orto(respuesta);
+// Otra(respuesta);
 
-let arr = ["pero", "la", "puta", "madre"];
+let arr = ["pero", "la", "coso", "masCoso"];
 
 // function print(arr){
 //     console.log(arr[2]);
